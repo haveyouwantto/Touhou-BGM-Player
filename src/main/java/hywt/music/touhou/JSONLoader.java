3ce88be86deb424055a205fcaeabac25e6e9687e
haveyouwantto <haveyouwantto@gmail.com>
2022-06-23 23:13:27 +0800

    fix charset

--- src/main/java/hywt/music/touhou/JSONLoader.java
+++ src/main/java/hywt/music/touhou/JSONLoader.java
@@ -9,14 +9,15 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class JSONLoader {
 
-    public static BGMData readBGMData() throws IOException {
+    public static BGMData readBGMData() {
         Gson g = new Gson();
-        Reader json = new InputStreamReader(Objects.requireNonNull(ClassLoader.getSystemResourceAsStream("BGM.json")));
+        Reader json = new InputStreamReader(Objects.requireNonNull(ClassLoader.getSystemResourceAsStream("BGM.json")), StandardCharsets.UTF_8);
         BGMData bgmData = g.fromJson(json, BGMData.class);
         bgmData.order = new ArrayList<>();
         for (int i = 0; i < bgmData.games.size(); i++) {
